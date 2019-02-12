c5a16fced8112e2e749e6d1ac3c9cef26163ed04
Joseph Muscari <joseph.muscari@deakin.edu.au>
2019-02-12 13:20:15 +1100

    Initial commit of application files

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    name: "Windstorm"
+  },
+  {
+    id: 2,
+    name: "Narco"
+  },
+  {
+    id: 3,
+    name: "Tornado"
+  },
+  {
+    id: 4,
+    name: "Magma"
+  },
+  {
+    id: 5,
+    name: "Pip"
+  }
+];
